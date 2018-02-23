430d86a0cf784f5c88c648c49d83ec750cac7299
msicakkanli <msicakkanli@protein.tech>
2018-02-23 21:49:57 +0300

    add getRandomQoute function & printqoute

--- js/script.js
+++ js/script.js
@@ -1,12 +1,8 @@
-
+// event listener to respond to "Show another quote" button clicks
+// when user clicks anywhere on the button, the "printQuote" function is called
 
 const qoutes = [
-    {
-        qoute: "I've missed more than 9000 shots in my career. I've lost almost 300 games. 26 times I've been trusted to take the game winning shot and missed. I've failed over and over and over again in my life. And that is why I succeed.",
-        source: "Michael Jordan",
-        year: 1988,
-    
-    },
+  
     {
         qoute: "Every strike brings me closer to the next home run.",
         source:  "Babe Ruth",
@@ -41,24 +37,44 @@ const qoutes = [
 ]
 //create random number
 function getRandomNumber() {
-    let randomNumber = Math.floor(Math.random() * 6 ) - 1 
+
+    let randomNumber = Math.floor(Math.random() * 5 ) 
     return randomNumber
 }
-const number = getRandomNumber();
+let number = getRandomNumber();
 
 //get the random number's qoute from object
-for (var key in qoutes) {
-    if (number == key ) {
-        var qoute = qoutes[key].qoute;
-       
-        console.log (qoute);
+
+
+
+function getRandomQoute(){
+    for (var key in qoutes) {
+        if (number == key ) {
+            var qoute = qoutes[key].qoute;
+            var source = qoutes[key].source;
+            var citation = qoutes[key].citation;
+            var year = qoutes[key].year;
+           
+            randomQoute = {};
+            randomQoute.qoute = qoute;
+            randomQoute.source = source;
+            randomQoute.citation = citation;
+            randomQoute.year = year;
+             
+        }
+    };
+    return randomQoute;
     }
-};
 
 
+function printQuote() {
+  let finalQoute =  getRandomQoute();
 
+    console.log(finalQoute) 
+}
 
-// event listener to respond to "Show another quote" button clicks
-// when user clicks anywhere on the button, the "printQuote" function is called
-document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+printQuote();
+
+//document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+
 
